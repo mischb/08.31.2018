320dffcef362a4911a41af674322140e72571905
mischb <mischarberlin@gmail.com>
2018-09-01 11:48:01 -0400

    added instructions and skeleton

---
+++ findPairForSum.js
@@ -0,0 +1,16 @@
+'use strict';
+// Given a list of non-negative integers and a target sum, find a pair of numbers that sums to the target sum.
+
+
+
+
+
+
+
+
+
+
+// example : 
+
+var pair = findPairForSum([3, 34, 4, 12, 5, 2], 9);
+console.log(pair); // --> [4, 5]
